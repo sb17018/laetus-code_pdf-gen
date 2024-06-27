9cb996f276398f4839d5e4d1fad081b3df6e1543
sb17018 <piotr.a.bar@gmail.com>
2024-06-27 20:40:28 +0200

    Create throwing PDF to browser

--- laetus-code_pdf-gen/src/main/java/com/yawer/laetus_code_pdf_gen_create/PdfCreator.java
+++ laetus-code_pdf-gen/src/main/java/com/yawer/laetus_code_pdf_gen_create/PdfCreator.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
@@ -23,24 +21,33 @@ import com.yawer.laetus_code_bars_gen.BarCombinationGen;
 import com.yawer.laetus_code_bars_gen.CodeType;
 
 public class PdfCreator {
+	
+	private final int CODE_VALUE;
+	private final CodeType CODE_TYPE;
+	private String filePath;
 
 	private final static float UNIT_CONVERTER = 2.8346456692913385826771653543307f; // 1mm / (1/72) inch
+	
+	public PdfCreator(int codeValue, CodeType codeType) {
+		this.CODE_VALUE = codeValue;
+		this.CODE_TYPE = codeType;
+		this.filePath = "src/main/resources/pdfs/Pharmacode_" + codeValue + "_" + codeType + ".pdf";
+	}
 
-	public static void createPdf(int codeValue, CodeType codeType)
+	public File createPdf()
 			throws IOException, DocumentException, InterruptedException {
 
-		File file = new File("src/main/resources/pdfs/Pharmacode_" + codeValue + "_" + codeType + ".pdf");
+		File file = new File(this.filePath);
 
 		if (!file.exists()) {
 
 			Document document = new Document();
-			int pageWidth = 148;
-			int pageHeight = 105;
+			int pageWidth = 105;
+			int pageHeight = 148;
 			Rectangle pageSize = new Rectangle(pageWidth * UNIT_CONVERTER, pageHeight * UNIT_CONVERTER);
 			document.setPageSize(pageSize);
 
-			PdfWriter pdfWriter = PdfWriter.getInstance(document,
-					new FileOutputStream("src/main/resources/pdfs/Pharmacode_" + codeValue + "_" + codeType + ".pdf"));
+			PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(this.filePath));
 
 			document.open();
 
@@ -53,7 +60,7 @@ public class PdfCreator {
 			cb.setColorFill(new CMYKColor(0f, 0f, 0f, 1f));
 
 			// adding bars combinations
-			BarCombinationGen barsCombGen = new BarCombinationGen(codeValue, codeType);
+			BarCombinationGen barsCombGen = new BarCombinationGen(this.CODE_VALUE, this.CODE_TYPE);
 			List<Bar> barsComb = barsCombGen.getBarsCombination();
 			float x = (pageWidth - barsCombGen.getCodeWidth()) * UNIT_CONVERTER / 2;
 			float y = (pageHeight - barsCombGen.getCodeHeight()) * UNIT_CONVERTER / 2 - 20;
@@ -73,15 +80,15 @@ public class PdfCreator {
 					BaseFont.EMBEDDED);
 			cb.beginText();
 			cb.setFontAndSize(bf, fontSize);
-			cb.moveText(25, 250);
+			cb.moveText(25, 370);
 			cb.showText("Pharmacode:");
 			cb.endText();
 
 			fontSize = 18;
 			cb.beginText();
 			cb.setFontAndSize(bf, fontSize);
-			String codeTypeFormatted = StringUtils.capitalize(codeType.name().toLowerCase());
-			cb.moveText((pageWidth * UNIT_CONVERTER - bf.getWidthPoint(codeTypeFormatted, fontSize)) / 2, 180);
+			String codeTypeFormatted = this.CODE_TYPE.name();
+			cb.moveText((pageWidth * UNIT_CONVERTER - bf.getWidthPoint(codeTypeFormatted, fontSize)) / 2, 270);
 			cb.showText(codeTypeFormatted);
 			cb.endText();
 
@@ -90,19 +97,14 @@ public class PdfCreator {
 					BaseFont.EMBEDDED);
 			cb.beginText();
 			cb.setFontAndSize(bfb, fontSize);
-			cb.moveText((pageWidth * UNIT_CONVERTER - bf.getWidthPoint(String.valueOf(codeValue), fontSize)) / 2, 50);
-			cb.showText(String.valueOf(codeValue));
+			cb.moveText((pageWidth * UNIT_CONVERTER - bf.getWidthPoint(String.valueOf(this.CODE_VALUE), fontSize)) / 2, 70);
+			cb.showText(String.valueOf(this.CODE_VALUE));
 			cb.endText();
 
 			cb.restoreState();
 
 			document.close();
 		}
-
-		// credits to https://stackoverflow.com/users/1300472/amit
-		Process pr = Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + file);
-		
-		// by Piotr Bar
-		if(pr.exitValue() == 0)file.delete();
+		return file;
 	}
 }
